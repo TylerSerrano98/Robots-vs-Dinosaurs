a56a7d0c17db735d57419ee1033c483aabb50157
TylerSerrano98 <91335774+TylerSerrano98@users.noreply.github.com>
2021-10-07 15:16:34 -0500

    set up dinosaur turn and show robo opponent options methodsto determine attacks for the dinosaurs side, and hit points for the robots side.

--- battlefield.py
+++ battlefield.py
@@ -1,3 +1,5 @@
+import random
+
 from fleet import Fleet
 from herd import Herd
 
@@ -8,7 +10,13 @@ class Battlefield:
         self.user_team = ''
         self.users_characters = None
         self.users_targets = None
-
+        self.user_attack = None
+        self.target_attack = None
+        self.user_defend = None
+        self.target_defend = None
+        self.user_selector = 0
+        self.target_selector = 0
+        self.target = 0
 
 
     def run_game(self):
@@ -24,18 +32,18 @@ class Battlefield:
         if self.user_team == 'dinosaurs':
             self.users_characters = self.herd.dinosaur
             self.users_targets = self.fleet.robot
-            print("/nYou're team of dinos is:")
+            print("\nYou're team of dinos is:")
             for dinosaur in self.users_characters:
-                print(f'Species: {dinosaur.species}, Health: {dinosaur.health_points}, Species: {dinosaur.species}')
+                print(f'Species: {dinosaur.species}, Health: {dinosaur.health_points}, Attack Power: {dinosaur.attack_power}')
             print("Here is your enemy robots:")
             for robot in self.users_targets:
-                print(f'Name: {robot.name}, Health: {robot.health}, Weapon: {robot.weapon.weapon_type}')         
+                print(f'Name: {robot.name}, Health: {robot.health}, Weapon: {robot.weapon.weapon_type} (atk) {robo.weapon.attack_power}')         
         else:
             self.user_team = self.fleet.robot
             self.users_targets = self.herd.dinosaur
-            print("/nYou're team of robots is:")
+            print("\nYou're team of robots is:")
             for robot in self.users_characters:
-                print(f'Name: {robot.name}, Health: {robot.health}, Weapon: {robot.weapon.weapon_type}')
+                print(f'Name: {robot.name}, Health: {robot.health}, Weapon: {robot.weapon.weapon_type} (atk) {robot.weapon.attack_power}')
             print("Here is your enemy dinosaurs:")
             for dinosaur in self.users_targets:
                 print(f'Species: {dinosaur.species}, Health: {dinosaur.health_points}, Species: {dinosaur.species}')
@@ -48,15 +56,40 @@ class Battlefield:
         pass
 
     def dino_turn(self):
-        pass
+        if self.users_characters:
+            self.user_attack = self.users_characters[self.user_selector]
+            print(f"\nYou're attacking character is: Species: {self.user_attack.species}, Health: {self.user_attack.health_points}, Attack Power: {self.user_attack.attack_power}")
+            print(f'\nPick a target to attack!\n')
+            self.show_robo_opponent_options()
+            self.target_selector = int(input("Enter enemy number:"))
+            while (self.target_selector < 0) or (self.target_selector > len(self.users_targets) - 1):
+                self.target_selector = int(input("not an enemy: please select an enemy number"))
+            self.target_defend = self.users_targets[self.target_selector]
+            print(f"\n{self.user_attack.species} hits {self.target_defend.name} for {self.user_attack.attack_power} health points!")
+            self.user_attack.attack(self.target_defend)
+            if self.user_selector >= (len(self.users_characters)-1):
+                self.user_selector = 0
+            else:
+                self.user_selector += 1
+        else:
+           self.target = random.randint(0, len(self.user)-1)
+           self.target_attack = self.users_targets[random.randint(0, len(self.enemy)-1)]
+           self.user_defend = self.users_characters[self.target]
+           print(f"\n{self.target_attack.species} attacks {self.user_defend.name} for {self.target_attack.attack_power} health points")
+           self.target_attack.attack(self.user_attack)
+           if self.user_selector >= (len(self.users_characters)-1):
+               self.user_selector = 0
 
+    def show_robo_opponent_options(self):
+        for robot in self.users_targets:
+            print(f'Enter "{self.target_selector}" Name: {robot.name}, Health: {robot.health}, Weapon: {robot.weapon.weapon_type} (atk) {robot.weapon.attack_power}')
+            self.target_selector += 1
+        
+    
     def robo_turn(self):
         pass
 
     def show_dino_opponent_options(self):
-        pass
-
-    def show_robo_opponent_options(self):
         pass
 
     def display_winners(self):
