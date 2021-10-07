665f37b09f93a547ac2c77ad8d2d42f6e58daa47
TylerSerrano98 <91335774+TylerSerrano98@users.noreply.github.com>
2021-10-07 15:50:17 -0500

    robo turn and show dino opponent options methods are both builtresults same as dino turn, and show robo opponent optionns.

--- battlefield.py
+++ battlefield.py
@@ -56,7 +56,7 @@ class Battlefield:
         pass
 
     def dino_turn(self):
-        if self.users_characters:
+        if self.users_characters == self.herd.dinosaur:
             self.user_attack = self.users_characters[self.user_selector]
             print(f"\nYou're attacking character is: Species: {self.user_attack.species}, Health: {self.user_attack.health_points}, Attack Power: {self.user_attack.attack_power}")
             print(f'\nPick a target to attack!\n')
@@ -87,10 +87,35 @@ class Battlefield:
         
     
     def robo_turn(self):
-        pass
+        if self.users_characters == self.fleet.robot:
+            self.user_attack = self.users_characters[self.user_selector]
+            print(f"\nYou're attacking character is: Name: {self.user_attack.name}, Health: {self.user_attack.health}, Weapon: {self.user_attack.weapon.weapon_type} (atk) {self.user_attack.weapon.attack_power}")
+            print(f'\nPick a target to attack!\n')
+            self.show_dino_opponent_options()
+            self.target_selector = int(input("Enter enemy number:"))
+            while (self.target_selector < 0) or (self.target_selector > len(self.users_targets) - 1):
+                self.target_selector = int(input("not an enemy: please select an enemy number"))
+            self.target_defend = self.users_targets[self.target_selector]
+            print(f"\n{self.user_attack.name} hits {self.target_defend.species} for {self.user_attack.weapon.attack_power} health points!")
+            self.user_attack.attack(self.target_defend)
+            if self.user_selector >= (len(self.users_characters)-1):
+                self.user_selector = 0
+            else:
+                self.user_selector += 1
+        else:
+            self.target = random.randint(0, len(self.user)-1)
+            self.target_attack = self.users_targets[random.randint(0, len(self.enemy)-1)]
+            self.user_defend = self.users_characters[self.target]
+            print(f"\n{self.target_attack.species} attacks {self.user_defend.name} for {self.target_attack.attack_power} health points")
+            self.target_attack.attack(self.user_attack)
+            if self.user_selector >= (len(self.users_characters)-1):
+                    self.user_selector = 0
+        
 
     def show_dino_opponent_options(self):
-        pass
+        for dinosaur in self.users_targets:
+            print(f'Enter "{self.target_selector}" Species: {dinosaur.species}, Health: {dinosaur.health_points}, Attack Power: {dinosaur.attack_power}')
+            self.target_selector += 1
 
     def display_winners(self):
         pass
